f4a4abaea5a5a5cc1b7ee47690e3639ed5a149ec
PavelETU <pavelsuvit@gmail.com>
2022-08-10 23:27:31 +0100

    Zero the first element in CooleyTurkeyFFT.kt

--- app/src/main/java/com/abakan/electronics/one/thousand/utils/fourier_transform/CooleyTurkeyFFT.kt
+++ app/src/main/java/com/abakan/electronics/one/thousand/utils/fourier_transform/CooleyTurkeyFFT.kt
@@ -8,7 +8,9 @@ import kotlin.math.sin
 
 class CooleyTurkeyFFT @Inject constructor() : FourierTransform {
     override fun transformToFrequencyDomain(bytes: ByteArray) =
-        calculateFFTInComplexNumbers(bytes.map { it.toComplex() }).map { it.toDouble() }
+        calculateFFTInComplexNumbers(bytes.map { it.toComplex() }).mapIndexed { index, complexNumber ->
+            if (index == 0) 0.0 else complexNumber.toDouble()
+        }
 
     private fun calculateFFTInComplexNumbers(input: List<ComplexNumber>): List<ComplexNumber> {
         val n = input.size
@@ -17,14 +19,14 @@ class CooleyTurkeyFFT @Inject constructor() : FourierTransform {
         }
         val wn = ComplexNumber(cos(2 * PI / n), sin(2 * PI / n))
         var w = ComplexNumber(1.0, 0.0)
-        val a0 = List(n/2) { input[2 * it] }
-        val a1 = List(n/2) { input[2 * it + 1] }
+        val a0 = List(n / 2) { input[2 * it] }
+        val a1 = List(n / 2) { input[2 * it + 1] }
         val y0 = calculateFFTInComplexNumbers(a0)
         val y1 = calculateFFTInComplexNumbers(a1)
         val results = MutableList(n) { ComplexNumber(0.0, 0.0) }
-        for (k in 0 until n/2) {
+        for (k in 0 until n / 2) {
             results[k] = y0[k] + w * y1[k]
-            results[k + n/2] = y0[k] - w * y1[k]
+            results[k + n / 2] = y0[k] - w * y1[k]
             w *= wn
         }
         return results
--- app/src/test/java/com/abakan/electronics/one/thousand/utils/fourier_transform/CooleyTurkeyFFTShould.kt
+++ app/src/test/java/com/abakan/electronics/one/thousand/utils/fourier_transform/CooleyTurkeyFFTShould.kt
@@ -1,7 +1,9 @@
 package com.abakan.electronics.one.thousand.utils.fourier_transform
 
 import org.hamcrest.CoreMatchers
-import org.hamcrest.MatcherAssert
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Assert.assertTrue
+import org.junit.Ignore
 import org.junit.Test
 
 class CooleyTurkeyFFTShould {
@@ -10,16 +12,32 @@ class CooleyTurkeyFFTShould {
     fun `transform no input in time domain into no output in frequency domain`() {
         val byteArray = ByteArray(4) { 0 }
         val results = fourierTransform.transformToFrequencyDomain(byteArray)
-        MatcherAssert.assertThat(results, CoreMatchers.`is`(listOf(0.0, 0.0, 0.0, 0.0)))
+        assertThat(results, CoreMatchers.`is`(listOf(0.0, 0.0, 0.0, 0.0)))
     }
 
     @Test
     fun `transform time domain into frequency domain zeroing first element`() {
         val byteArray = ByteArray(4) { (it + 1).toByte() }
         val results = fourierTransform.transformToFrequencyDomain(byteArray)
-        MatcherAssert.assertThat(results[0], CoreMatchers.`is`(10.0))
-        MatcherAssert.assertThat(results[1], CoreMatchers.`is`(2.8284271247461903))
-        MatcherAssert.assertThat(results[2], CoreMatchers.`is`(2.0))
-        MatcherAssert.assertThat(results[3], CoreMatchers.`is`(2.82842712474619))
+        assertThat(results[0], CoreMatchers.`is`(0.0))
+        assertThat(results[1], CoreMatchers.`is`(2.8284271247461903))
+        assertThat(results[2], CoreMatchers.`is`(2.0))
+        assertThat(results[3], CoreMatchers.`is`(2.82842712474619))
+    }
+
+    @Test
+    @Ignore("Ignoring due to the long execution time")
+    fun `should be at least 10 times faster than n^2 implementation for 8000 samples`() {
+        val simpleDFT = SimpleDFT()
+        val byteArray = ByteArray(8000) { it.toByte() }
+        var milliseconds = System.currentTimeMillis()
+        fourierTransform.transformToFrequencyDomain(byteArray)
+        val timeToTransformUsingCooleyTurkey = System.currentTimeMillis() - milliseconds
+        milliseconds = System.currentTimeMillis()
+        simpleDFT.transformToFrequencyDomain(byteArray)
+        val timeToTransformUsingQuadraticAlgorithm = System.currentTimeMillis() - milliseconds
+        println("It took simple algorithm $timeToTransformUsingQuadraticAlgorithm milliseconds to process ${byteArray.size} samples")
+        println("It took FFT algorithm $timeToTransformUsingCooleyTurkey milliseconds to process ${byteArray.size} samples")
+        assertTrue(timeToTransformUsingQuadraticAlgorithm > timeToTransformUsingCooleyTurkey * 10)
     }
 }
